cb8c4dde74f5bf7c7d779cd03204c63dced3ceab
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-06 23:29:03 +0100

    question1 done

---
+++ objects_snacks.js
@@ -0,0 +1,3 @@
+// Code Question 1
+
+// Il riferimento è lo stesso, quindi in console viene stampato due volte "Double cheese burger", perchè modificando secondBurger viene modificato anche hamburger
