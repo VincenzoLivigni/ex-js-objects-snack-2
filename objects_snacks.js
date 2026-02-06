64002b3ea8584eca3609acabf52450be687dda8d
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-06 23:35:51 +0100

    question2 done

--- objects_snacks.js
+++ objects_snacks.js
@@ -1,3 +1,9 @@
 // Code Question 1
 
 // Il riferimento è lo stesso, quindi in console viene stampato due volte "Double cheese burger", perchè modificando secondBurger viene modificato anche hamburger
+
+
+
+// Code Question 2
+
+// in console vengono stampati due oggetti grazie allo spred operator, ma il riferimento ad "ingredients" è sempre lo stesso, di conseguenza viene stampato due volte "salad"
